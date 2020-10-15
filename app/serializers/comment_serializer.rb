17c463d6eaaf228de3d0e13bd7828a4490b3e9df
allenorton <norton.allene@gmail.com>
2020-10-14 19:44:07 -0500

    edited serializer relationships

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :wine_id, :body
+  attributes :id, :user_id, :wine_id, :body, :username
+  belongs_to :user
+  belongs_to :wine
+
+  def username
+    object.user.display_name
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :uid, :email, :display_name, :first_name, :last_name, :image, :location
 
   has_many :wines
+  has_many :comments
 end
