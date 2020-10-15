5b3931235ec0022901184605d9565158a45287e9
allenorton <norton.allene@gmail.com>
2020-10-15 12:15:21 -0500

    fixed destroy user wine action

--- app/controllers/users_wines_controller.rb
+++ app/controllers/users_wines_controller.rb
@@ -4,20 +4,9 @@ class UsersWinesController < ApplicationController
         render json: usersWines
     end
 
-    # def create
-    #     user = User.where(uid: params[:user_id]).first
-    #     wine = Wine.where(spn_id: params[:wine_id].to_s).first
-    #     # byebug
-    #     userWine = UsersWine.find_or_create_by(
-    #         user_id: user.id,
-    #         wine_id: wine.id)
-    #     render json: userWine
-    # end
-
-
-    # private
-
-    # def users_wine_params
-    #     params.require(:users_wine).permit(:user_id, :wine_id)
-    # end
+    
+    def destroy
+        wine = UsersWine.find_by(wine_id: params[:id])
+        wine.destroy
+    end
 end
