d1840678b10e0f952a080694636c9e40a2d9cbf3
allenorton <norton.allene@gmail.com>
2020-10-14 22:02:32 -0500

    post comment is working, need to change saved wines method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,6 @@ class CommentsController < ApplicationController
 
     private 
     def comment_params
-        params.require(:comment).permit(:user_id, :comment_id, :body)
+        params.require(:comment).permit(:user_id, :wine_id, :body)
     end
 end
