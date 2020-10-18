281d1415602b0b2d10fcb8ea9034d61cf040445b
allenorton <norton.allene@gmail.com>
2020-10-18 15:09:04 -0500

    added delete functionality for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,9 +14,15 @@ class CommentsController < ApplicationController
         render json: comment
     end
 
+    def destroy
+        comment = Comment.find_by(id: params[:id])
+        # byebug
+        comment.destroy
+    end
+
 
     private 
     def comment_params
-        params.require(:comment).permit(:user_id, :wine_id, :body)
+        params.require(:comment).permit(:id, :user_id, :wine_id, :body)
     end
 end
