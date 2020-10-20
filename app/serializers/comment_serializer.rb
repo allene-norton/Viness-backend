e461ebff68950705e41f9562b1573a234e495326
allenorton <norton.allene@gmail.com>
2020-10-20 12:55:31 -0500

    added user image to comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,9 +1,13 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :wine_id, :body, :username
+  attributes :id, :user_id, :wine_id, :body, :username, :img
   belongs_to :user
   belongs_to :wine
 
   def username
     object.user.display_name
+  end
+
+  def img
+    object.user.image
   end
 end
